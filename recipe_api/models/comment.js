ecdf21043ea77b485b1a11172c36893bcadd95ff
miray <tepemiray95@gmail.com>
2024-04-05 22:05:00 +0300

    commit

---
+++ recipe_api/models/comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    meal: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Meal',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
---
+++ recipe_api/models/recipe.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
